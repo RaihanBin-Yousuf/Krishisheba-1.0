810978c0a54953262ad2512d2723ed0122eccd7b
RaihanBin-Yousuf <raihanuddin200983@gmail.com>
2021-04-28 12:04:33 +0600

    index.js update

--- resources/js/frontend/home/Index.js
+++ resources/js/frontend/home/Index.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
+import AboutUs from '../includes/AboutUs';
+import Contact from '../include/Contact';
+import Cta from '../includes/Cta';
+import Faq from '../includes/Faq';
+import Footer from '../includes/Footer';
 import Hero from '../includes/Hero';
+import HowItsWorks from '../includes/HowItsWorks';
+import Service from '../includes/Service';
+import TabSlider from '../includes/TabSlider';
+import Team from '../includes/Team';
+import TopBarAndHeader from '../includes/TopBarAndHeader';
 import TopCategorySlider from '../includes/TopCategorySlider';
+import TopBarAndHeader from '../includes/TopBarAndHeader';
 export default class Index extends Component {
     render() {
         return (
             <div>
+                <AboutUs/>
+                <Contact/>
+                <Cta/>
+                <Faq/>
+                <Footer/>
                 <Hero/>
-                <TopCategorySlider/>
+                <HowItsWorks/>
+                <Service/>
+                <TabSlider/>
+                <Team/>
+                <Service/>
+                <TopBarAndHeader/>
+                <TopCategorySlider/>              
             </div>
         )
     }
